b0b67dd690fc3d5ef68c552d6cfa37575f563533
최수환 <frcp9408@gmail.com>
2023-10-11 10:33:15 +0900

    initial commint

---
+++ SpringBoot2/src/main/java/com/smhrd/player/controller/PlayerController.java
@@ -0,0 +1,37 @@
+package com.smhrd.player.controller;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.smhrd.player.service.PlayerService;
+
+@RestController // 리액트 서버로 데이터만 응답
+@CrossOrigin("http://localhost:3000")
+public class PlayerController {
+
+	@Autowired
+	private PlayerService playerService;
+
+	@RequestMapping("/PlayerList.do")
+	public JSONArray PlayerList() {
+		System.out.println("스프링 : PlayerList.do");
+		// 리액트 서버로 json데이터 응답
+		return playerService.PlayerList();
+	}
+
+	@RequestMapping("PlayerDetail.do")
+	public JSONObject PlayerDetail(@RequestParam String name) {
+		System.out.println("이름 확인 : " + name);
+		
+		// DB에서 나상호의 정보만 가지고와서 리액트로 전송
+		// JsonObject -> put 메서드 활용
+		
+		return null;
+	}
+
+}
---
+++ SpringBoot2/src/main/java/com/smhrd/player/service/PlayerService.java
@@ -0,0 +1,74 @@
+package com.smhrd.player.service;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.json.simple.JSONArray;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Service;
+
+import com.smhrd.player.converter.ImageToBase64;
+import com.smhrd.player.converter.PlayerImageConverter;
+import com.smhrd.player.entity.Player;
+import com.smhrd.player.mapper.PlayerMapper;
+
+@Service
+public class PlayerService {
+
+	@Autowired
+	private PlayerMapper playerMapper;
+
+	@Autowired
+	private ResourceLoader resourceLoader; // 특정 경로에 있는 파일을 불러오기위한 객체
+
+	public JSONArray PlayerList() {
+		List<Player> p_list = playerMapper.PlayerList();
+
+		JSONArray jsonPlayerList = new JSONArray();
+
+		// DB에서 가지고 온 선수의 정보 중에서 imgSrc는 실제경로, 실제파일이 아님
+		// React 서버에 이미지를 직접 넘겨야 하기 때문에 이미지 데이터 가공
+		// imgSrc 파일 이름 -> 실제 파일 -> 이미지(byte형태의 문자열)
+
+		PlayerImageConverter<File, String> converter = new ImageToBase64();
+		for (Player p : p_list) {
+			// 1. 파일이름 -> 실제파일 -> byte문자열
+			// 1-1. 파일이름 -> 실제 파일
+			// classpath : src/main/resources
+			String filePath = "classpath:/static/player_img/" + p.getImgSrc();
+			/* String filePath = "static/player_img/" + p.getImgSrc(); */
+			System.out.println(filePath);
+
+			// 1-2. 실제 파일로 불러오기
+			// Resource resource = resourceLoader.getResource(filePath);
+			File TempFile = null;
+			String fileString = null;
+			try {
+				// 배포 시 파일로 불러오면 파일을 못찾기 때문에 inputStream으로 진행
+				InputStream inputStream = new ClassPathResource(filePath).getInputStream();
+				TempFile = File.createTempFile(p.getImgSrc(), "");
+				FileUtils.copyInputStreamToFile(inputStream, TempFile);
+				// 1-3. converter를 통해서 byte문자열로 변환
+				fileString = converter.convert(TempFile);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			// System.out.println("fileString 확인 : " + fileString);
+
+			// 2. imgSrc 필드 : 원래 파일 이름 -> byte 문자열
+			p.setImgSrc(fileString);
+
+			// 3. Json 형태로 변환(JsonArray)
+
+			jsonPlayerList.add(p);
+		}
+
+		return jsonPlayerList;
+	}
+
+}
